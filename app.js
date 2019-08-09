2ec36bac56820daeabbb29debc914f1106af683d
Ning Zeng <ningzeng@NingdeMacBook-Pro.local>
2019-08-09 23:00:07 +0800

    add ffmpeg

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let express = require("express"),
     routes = require("./routes"),
     config = require("./config"),
     logger = require("morgan"),
+    ffmpeg = require("ffmpeg"),
     app = express();
 
 app.set("views", "./views");
@@ -24,6 +25,27 @@ app.use(connection(mysql, config.getDbConfig(), "request"));
 app.get("/", function(req, res) {
     res.send("video server");
 });
+
+//获取视频缩略图
+function getVideoSceenshots(videoPath, outPutPath, frameRate, frameCount) {
+    const process = new ffmpeg(videoPath);
+    return process.then(function(video) {
+        video.fnExtractFrameToJPG(outPutPath, {
+            frame_rate: frameRate,
+            number: frameCount,
+            file_name: 'my_frame_%t_%s'
+        }, function(error, files) {
+            if (!error)
+                console.log('Frames: ' + files)
+        })
+    }, function(err) {
+        console.log('Error: ' + err)
+    })
+}
+
+const videoPath = './public/upload/002.mp4';
+const outputPath = './public/';
+getVideoSceenshots(videoPath, outputPath, 1, 5)
 
 routes(app);
 
