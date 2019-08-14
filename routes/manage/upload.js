32edd5b854a551eb39a5947a45d4c9450dc08486
Ning Zeng <ningzeng@NingdeMacBook-Pro.local>
2019-08-14 22:48:08 +0800

    add file api

--- routes/manage/upload.js
+++ routes/manage/upload.js
@@ -1,88 +1,221 @@
 let express = require("express"),
-  router = express.Router(),
-  multer = require("multer"),
-  fs = require("fs"),
-  storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, "./public/upload");
-    },
-    filename: function(req, file, cb) {
-      // console.log('name',file.originalname);
-      // var str = file.originalname.split('.');
-      cb(null, file.originalname);
-    }
-  }),
-  upload = multer({ storage: storage });
+    router = express.Router(),
+    multer = require("multer"),
+    fs = require("fs"),
+    storage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, "./public/upload");
+        },
+        filename: function(req, file, cb) {
+            // console.log('name',file.originalname);
+            // var str = file.originalname.split('.');
+            cb(null, file.originalname);
+        }
+    }),
+    upload = multer({ storage: storage });
 
 var uploadfile = router.route("/uploadfile");
 
 uploadfile.post(upload.array("file", 20), function(req, res, next) {
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
-    let arr = [];
-    for (let i in req.files) {
-      console.log("file", JSON.stringify(req.files));
-      let metadata = [
-        req.files[i].fieldname,
-        req.files[i].originalname,
-        req.files[i].encoding,
-        req.files[i].mimetype,
-        req.files[i].size,
-        req.files[i].path,
-        new Date()
-      ];
-      arr.push(metadata);
-    }
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
+        let arr = [];
+        for (let i in req.files) {
+            console.log("file", JSON.stringify(req.files));
+            let metadata = [
+                req.files[i].fieldname,
+                req.files[i].originalname,
+                req.files[i].encoding,
+                req.files[i].mimetype,
+                req.files[i].size,
+                req.files[i].path,
+                new Date()
+            ];
+            arr.push(metadata);
+        }
 
-    let addSQL =
-      "INSERT INTO uploadfiles(fieldname, filename, encoding, mimetype, size, filepath, addTime) VALUES ?";
+        let addSQL =
+            "INSERT INTO uploadfiles(fieldname, filename, encoding, mimetype, size, filepath, addTime) VALUES ?";
 
-    conn.query(addSQL, [arr], function(err, rows) {
-      if (err) {
-        res.send({
-          code: 1,
-          desc: "上传数据库异常"
+        conn.query(addSQL, [arr], function(err, rows) {
+            if (err) {
+                res.send({
+                    code: 1,
+                    desc: "上传数据库异常"
+                });
+                return next("query error" + err);
+            } else {
+                // console.log('rows',rows);
+                // rows.insertId,rows.affectedRows,
+                res.send({
+                    code: 0,
+                    desc: "上传成功"
+                });
+            }
         });
-        return next("query error" + err);
-      } else {
-        // console.log('rows',rows);
-        // rows.insertId,rows.affectedRows,
-        res.send({
-          code: 0,
-          desc: "上传成功"
-        });
-      }
     });
-  });
 });
 
 let removefile = router.route("/removefile/filename/:filename");
 removefile.get(function(req, res, next) {
-  let fileName = req.params.filename;
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    let fileName = req.params.filename;
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    fs.unlink("./public/upload/" + fileName, err => {
-      if (!err) {
-        let sql = "delete from uploadfiles where filename= ?";
+        fs.unlink("./public/upload/" + fileName, err => {
+            if (!err) {
+                let sql = "delete from uploadfiles where filename= ?";
 
-        conn.query(sql, [fileName], function(err, rows) {
-          if (err) {
-            res.send({
-              code: 1,
-              desc: "查询数据库异常"
-            });
-            return next("query error" + err);
-          } else {
-            res.send({
-              code: 0,
-              desc: "删除成功"
-            });
-          }
+                conn.query(sql, [fileName], function(err, rows) {
+                    if (err) {
+                        res.send({
+                            code: 1,
+                            desc: "查询数据库异常"
+                        });
+                        return next("query error" + err);
+                    } else {
+                        res.send({
+                            code: 0,
+                            desc: "删除成功"
+                        });
+                    }
+                });
+            } else return next("delete error" + err);
         });
-      } else return next("delete error" + err);
     });
-  });
 });
+
+let adstorage = multer.diskStorage({
+        destination: function(req, file, cb) {
+            cb(null, "./public/ads");
+        },
+        filename: function(req, file, cb) {
+            // console.log('name',file.originalname);
+            // var str = file.originalname.split('.');
+            cb(null, file.originalname);
+        }
+    }),
+    adupload = multer({ storage: adstorage });
+
+var uploadAdFile = router.route("/uploadadfile");
+
+uploadAdFile.post(adupload.array("file", 20), function(req, res, next) {
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
+        let arr = [];
+        for (let i in req.files) {
+            // console.log("file", JSON.stringify(req.files));
+            let metadata = [
+                req.files[i].originalname,
+                req.files[i].encoding,
+                req.files[i].mimetype,
+                req.files[i].size,
+                req.files[i].path,
+                new Date()
+            ];
+            arr.push(metadata);
+        }
+
+        let addSQL = "INSERT INTO adfiles(filename, encoding, mimetype, size, filepath, addTime) VALUES ?";
+
+        conn.query(addSQL, [arr], function(err, rows) {
+            if (err) {
+                res.send({
+                    code: 1,
+                    desc: "上传数据库异常"
+                });
+                return next("query error" + err);
+            } else {
+                res.send({
+                    code: 0,
+                    desc: rows.insertId
+                });
+            }
+        });
+    });
+});
+
+let removeAdFile = router.route("/removead");
+removeAdFile.post(function(req, res, next) {
+    let id = req.query.id;
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
+        let querySql = "SELECT adfiles.filename FROM	adinfo	LEFT JOIN adfiles ON adinfo.picid = adfiles.id WHERE adinfo.id = ? ";
+        conn.query(querySql, [id], function(err, rows1) {
+            if (err) return next("query error" + err);
+            fs.unlink("./public/ads/" + rows1[0].filename, err => {
+                if (!err) {
+                    let sql = "DELETE adinfo.*,adfiles.* FROM	adinfo	LEFT JOIN adfiles ON adinfo.picid = adfiles.id WHERE adinfo.id = ? ";
+
+                    conn.query(sql, [id], function(err, rows) {
+                        if (err) {
+                            res.send({
+                                code: 1,
+                                desc: "query error"
+                            });
+                            return next("query error" + err);
+                        } else {
+                            res.send({
+                                code: 0,
+                                desc: "remove success"
+                            });
+                        }
+                    });
+                } else return next("delete error" + err);
+            });
+        });
+    });
+});
+
+let addAd = router.route("/addadinfo");
+addAd.post(function(req, res, next) {
+    let name = req.query.name,
+        position = req.query.position,
+        link = req.query.link,
+        picId = req.query.pid;
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
+        let sql = "INSERT INTO adinfo(`name`,position,link,picid) VALUES (?,?,?,?);";
+
+        conn.query(sql, [name, position, link, picId], function(err, rows) {
+            if (err) {
+                res.send({
+                    code: 1,
+                    desc: "add add fail"
+                });
+                return next("query error" + err);
+            } else {
+                res.send({
+                    code: 0,
+                    desc: rows.insertId
+                });
+            }
+        });
+    });
+});
+
+let getAds = router.route("/getadlist");
+getAds.get(function(req, res, next) {
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
+        let sql = "SELECT adinfo.*,adfiles.filename FROM	adinfo	LEFT JOIN adfiles ON adinfo.picid = adfiles.id";
+
+        conn.query(sql, [], function(err, rows) {
+            if (err) {
+                res.send({
+                    code: 1,
+                    desc: "add add fail"
+                });
+                return next("query error" + err);
+            } else {
+                res.send({
+                    code: 0,
+                    desc: rows
+                });
+            }
+        });
+    });
+});
+
 
 module.exports = router;
--- routes/manage/videolevel.js
+++ routes/manage/videolevel.js
@@ -1,185 +1,185 @@
 let express = require("express"),
- router = express.Router();
+    router = express.Router();
 
 let getLevel1 = router.route("/getlevel1");
 
 getLevel1.get(function(req, res, next) {
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "SELECT * FROM `level1`;";
+        let sql = "SELECT * FROM `level1`;";
 
-    conn.query(sql, [], function(err, rows) {
-      if (err) return next("get level1 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: rows
-        })
-      );
+        conn.query(sql, [], function(err, rows) {
+            if (err) return next("get level1 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: rows
+                })
+            );
+        });
     });
-  });
 });
 
 let addLevel1 = router.route("/addlevel1");
 
 addLevel1.post(function(req, res, next) {
 
-  let className = req.query.name;
+    let className = req.query.name;
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "INSERT INTO level1 ( className, level ) VALUES	( ? , 1 );";
+        let sql = "INSERT INTO level1 ( className, level ) VALUES	( ? , 1 );";
 
-    conn.query(sql, [className], function(err, rows) {
-      if (err) return next("add level1 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: rows.insertId
-        })
-      );
+        conn.query(sql, [className], function(err, rows) {
+            if (err) return next("add level1 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: rows.insertId
+                })
+            );
+        });
     });
-  });
 });
 
 let setLevel1 = router.route("/setlevel1");
 
 setLevel1.post(function(req, res, next) {
-  let className = req.query.name,
-  id = req.query.id;
+    let className = req.query.name,
+        id = req.query.id;
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "UPDATE level1 SET level1.className = ? WHERE id = ?;";
+        let sql = "UPDATE level1 SET level1.className = ? WHERE id = ?;";
 
-    conn.query(sql, [className,id], function(err, rows) {
-      if (err) return next("set level1 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: 'set level1 success'
-        })
-      );
+        conn.query(sql, [className, id], function(err, rows) {
+            if (err) return next("set level1 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: 'set level1 success'
+                })
+            );
+        });
     });
-  });
 });
 
 let removeLevel1 = router.route("/removelevel1");
 
 removeLevel1.post(function(req, res, next) {
-  let id = req.query.id;
+    let id = req.query.id;
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "DELETE level2,level1 FROM	level2	LEFT JOIN level1 ON level2.parent = level1.id WHERE	level1.id = ?;";
+        let sql = "DELETE level2,level1 FROM	level2	LEFT JOIN level1 ON level2.parent = level1.id WHERE	level1.id = ?;";
 
-    conn.query(sql, [id], function(err, rows) {
-      if (err) return next("remove level1 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: 'remove level1 success'
-        })
-      );
+        conn.query(sql, [id], function(err, rows) {
+            if (err) return next("remove level1 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: 'remove level1 success'
+                })
+            );
+        });
     });
-  });
 });
 
 let getLevel2 = router.route("/getlevel2");
 
 getLevel2.post(function(req, res, next) {
 
-  let id = req.query.id;
+    let id = req.query.id;
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "SELECT * FROM `level2` where parent = ?;";
+        let sql = "SELECT * FROM `level2` where parent = ?;";
 
-    conn.query(sql, [id], function(err, rows) {
-      if (err) return next("get level2 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: rows
-        })
-      );
+        conn.query(sql, [id], function(err, rows) {
+            if (err) return next("get level2 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: rows
+                })
+            );
+        });
     });
-  });
 });
 
 let addLevel2 = router.route("/addlevel2");
 
 addLevel2.post(function(req, res, next) {
 
-  let className = req.query.name,
-  parent = req.query.parent;
+    let className = req.query.name,
+        parent = req.query.parent;
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "INSERT INTO level2 ( className, level, parent ) VALUES	( ? , 2 , ?);";
+        let sql = "INSERT INTO level2 ( className, level, parent ) VALUES	( ? , 2 , ?);";
 
-    conn.query(sql, [className, parent], function(err, rows) {
-      if (err) return next("add level2 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: rows.insertId
-        })
-      );
+        conn.query(sql, [className, parent], function(err, rows) {
+            if (err) return next("add level2 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: rows.insertId
+                })
+            );
+        });
     });
-  });
 });
 
 let setLevel2 = router.route("/setlevel2");
 
 setLevel2.post(function(req, res, next) {
-  let className = req.query.name,
-  id = req.query.id;
+    let className = req.query.name,
+        id = req.query.id;
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "UPDATE level2 SET level2.className = ? WHERE id = ?;";
+        let sql = "UPDATE level2 SET level2.className = ? WHERE id = ?;";
 
-    conn.query(sql, [className,id], function(err, rows) {
-      if (err) return next("set level2 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: 'set level2 success'
-        })
-      );
+        conn.query(sql, [className, id], function(err, rows) {
+            if (err) return next("set level2 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: 'set level2 success'
+                })
+            );
+        });
     });
-  });
 });
 
 let removeLevel2 = router.route("/removelevel2");
 
 removeLevel2.post(function(req, res, next) {
-  let id = req.query.id;
+    let id = req.query.id;
 
-  req.getConnection(function(err, conn) {
-    if (err) return next(err);
+    req.getConnection(function(err, conn) {
+        if (err) return next(err);
 
-    let sql = "DELETE FROM level2 WHERE id = ?;";
+        let sql = "DELETE FROM level2 WHERE id = ?;";
 
-    conn.query(sql, [id], function(err, rows) {
-      if (err) return next("remove level2 error" + err);
-      res.send(
-        JSON.stringify({
-          code: 0,
-          desc: 'remove level2 success'
-        })
-      );
+        conn.query(sql, [id], function(err, rows) {
+            if (err) return next("remove level2 error" + err);
+            res.send(
+                JSON.stringify({
+                    code: 0,
+                    desc: 'remove level2 success'
+                })
+            );
+        });
     });
-  });
 });
 
 module.exports = router;
