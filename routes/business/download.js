66be387a82c8eb5a164f5edf76f7230a8e363626
whutzn <whutzn@foxmail.com>
2019-08-26 23:06:14 +0800

    fix download

--- routes/business/download.js
+++ routes/business/download.js
@@ -1,11 +1,16 @@
 var express = require("express");
 var router = express.Router();
 
-// user login
+// 
 var download = router.route("/download");
 
 download.get(function(req, res) {
     res.download("./public/apk/laoyouquan.apk");
 });
 
+var download1 = router.route("/download1");
+
+download1.get(function(req, res) {
+    res.download("./public/apk/laoyouquan.ipa");
+});
 module.exports = router;
